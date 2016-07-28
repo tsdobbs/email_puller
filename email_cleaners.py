87074b78506e15ffb267848fe91afc57f647994f
tsdobbs <tim.dobbs@gmail.com>
2016-07-27 17:02:16 -0700

    Restructed for readability. Bug fixes and improvements. Added .gitignore for config.txt

--- email_cleaners.py
+++ email_cleaners.py
@@ -1,57 +1,85 @@
 import re
 import BeautifulSoup
 
-#replaces html codes with the characters they represent, since it's safe it use the actual characters in plaintext
+
 def htmlUnescape(m):
+    """ Replaces HTML codes with the characters they represent, since it's safe it use the actual characters in plaintext """
     try:
         return unichr(int(m.group(1), 10))
     except:
         return m.group()
 
-#replaces all hyperlinks in emails with ' some website ', since it's not super meaningful for data analysis
-#When we look for hyperlinks, we say URLs end with a space, and can extend over newlines (because they often do in emails)
-#   This leads to sometimes accidentally grabbing the word right after the URL if there was only a newline.
-#   The solution implemented is to add back any string at the end of a URL that comes after a newline and has no slashes (i.e. Looks like a word)
-#   Sometimes you'll get the end of a URL added back in, but oh well
+
 def replace_hyperlinks(m):
+    """
+    Replaces all hyperlinks in emails with ' some website ', since it's not super meaningful for data analysis
+    When we look for hyperlinks, we say URLs end with a space, and can extend over newlines (because they often do in
+     emails). This leads to sometimes accidentally grabbing the word right after the URL if there was only a newline.
+     The solution implemented is to add back any string at the end of a URL that comes after a newline and has no
+     slashes (i.e. Looks like a word). Sometimes you'll get the end of a URL added back in, but oh well
+    :param m: a re.match() object where we found a hyperlink inside some text
+    :return: either the string ' some_website ' or ' some_website ' followed by the very end of the re.match() object
+    """
     try:
         return ' some website ' + m.group()[re.search('\n[^/]* ',m.group()).start():]
     except:
         return ' some_website '
 
-# This part gets rid of any previous emails that were appended to the end of the email we're currently looking at
-# It looks for a Regex along the lines of "On [some date], [someone] wrote:", as well as a number of other common email delimiters
+
 def strip_replies(payload):
+    """
+    Gets rid of any previous emails that were appended to the end of the email we're currently looking at
+     Creates a list of common indicators email services use to denote a reply has started, then checks for those
+     in the email and deletes anything after them.
+    :param payload: the content of the email in question, including replies and often HTML escapes and other junk
+    :return payload: email content with (theoretically) all of the appended replies removed
+    """
     match_list = list()
+    # In HTML emails sometimes the replies live inside their own div, which is handy
     match_list.append('<div[^>]*class=[^>]*"(gmail|yahoo)_(extra|quoted?)"[^>]*>')
-    #This is the more specific version of the GMail reply line. Included here just incase there's a line break at the end of the line, which happens sometimes
-    match_list.append('(?s)On( [A-Z][a-z]{2},)? [A-Z][a-z]{2} [0-9]{1,2}, [0-9]{4},? (at )?[0-9]{1,2}:[0-9]{2} [AP]M,.*wrote:')
     match_list.append('On .* wrote:')
+    # Below is the more specific version of the Gmail reply line.
+    # Included here just incase there's a line break at the end of the line, which happens sometimes
+    match_list.append('(?s)On( [A-Z][a-z]{2},)? [A-Z][a-z]{2} [0-9]{1,2}, [0-9]{4},? (at )?[0-9]{1,2}:[0-9]{2} [AP]M,.*wrote:')
     match_list.append('(?m)^-- ?$')
     match_list.append('(?i)-* *(Original|Forwarded) Message *-*')
     match_list.append('(?m)^_{32}')
     match_list.append('(?m)^From: ')
     match_list.append('(?m)^(<[^>]*>)?Sent from my.*')
-    #This one is very aggressive - if a line starts with '>' we say that now it's a reply, but the philosophy here is that we'd rather lose some good data than allow any bad data in.
+    # Below is aggressive, but the philosophy here is that we'd rather lose some good data than allow any bad data in.
     match_list.append('(?m)^(<[^>]*>)?>')
+
     for match_string in match_list:
         if re.search(match_string, payload):
             reply_start = re.search(match_string, payload).start()
             payload = payload[:reply_start]
     return payload
 
-#This part removes all the html tags, as well as a number of common html escape codes and long URLs
+
 def strip_html(html_payload):
-    html_payload = html_payload.replace('=\r\n','')  # line breaks need to be replaced first because they can be in the middle of the others
+    """
+    Strips replies with strip_replies, then removes all the html tags, as well as a number of common html escape codes,
+     long URLs, and other things that don't make for useful reading in a large dataset.
+    :param html_payload: The content of an email, including all HTML and any appended replies
+    :return text_payload: The content of an email with all HTML tags and appended replies stripped out
+    """
+    # Line breaks removed first because they can be in the middle of the other escapes or codes, disrupting the search
+    html_payload = html_payload.replace('=\r\n','')
+
     html_payload = strip_replies(html_payload)
-    html_payload = html_payload.replace('<br>','=C2=A0\r\n')
+
+    html_payload = html_payload.replace('<br>','=C2=A0\r\n') # Preserve HTML line breaks in plaintext version
     html_payload = html_payload.replace('</div><div>', '=C2=A0\r\n')
+
     soup = BeautifulSoup.BeautifulSoup(html_payload)
-    text_payload = soup.text
+    text_payload = soup.text # Remove all HTML tags
+
     text_payload = re.sub('&#([^;]+);', htmlUnescape, text_payload)
     text_payload = re.sub('(?ms)https?://[^ ]*( |$)', replace_hyperlinks, text_payload)
 
-    replacement_dict = {'(=([A-Z]|[0-9]){2})+':' ', '&lt;':'<', '&gt;':'>', '&quot;':'"'}
+    # We iterate over a dictionary of HTML escapes to replace and what to replace them with. Can be added to!
+    # Included in the dict is dropping any time we see >100 characters without a space. It's unlikely that's a word.
+    replacement_dict = {'(=([A-Z]|[0-9]){2})+':' ', '&lt;':'<', '&gt;':'>', '&quot;':'"', '(?s)[^ ]{100,}':''}
     for key in replacement_dict:
         text_payload = re.sub(key, replacement_dict[key], text_payload)
 
--- main.py
+++ main.py
@@ -2,61 +2,107 @@ import imaplib
 from email.parser import Parser
 import email_cleaners
 
-#pulls apart the payload object to get the actual email text, then cleans that text so it can be written to a textfile
 def payload_parser(payload):
+    """
+    Pulls apart the payload object to get the actual email text, then cleans that text so it can be written to a textfile
+    :param payload:
+    :return parsed_payload:
+    """
     try:
         parsed_payload = ''
-        #Sometimes the payload object is a list of payload parts, e.g. [plaintext, htmltext, maybe an atttachment]
-        #This for-loop iterates over the payload object until it finds something that returns a string of text, which we assume is probably the text we're looking for
-        #The loop is happy with the first text it gets back. It might make more sense to prefer the plaintext part or the html part - something to consider changing.
+        """
+        Sometimes the payload object is a list of payload parts, e.g. [plaintext, htmltext, maybe an atttachment]
+        This for-loop iterates over the payload object until it finds something that returns a string of text, which we assume is probably the text we're looking for
+        The loop is happy with the first text it gets back. It might make more sense to prefer the plaintext part or the html part - something to consider changing.
+        """
         if type(payload._payload) is list:
             for part in payload._payload:
                 parsed_payload = payload_parser(part)
                 if parsed_payload != '': break
         else:
-            #Pull the payload text and send it to functions that strip out replies and html junk
-            #Currently, we don't differentiate between how to process plaintext or html, because sometimes things labeled as plaintext still have html codes in them for some reason (email is frustrating and poorly standardized, you see)
-            #There might be some optimization to be done in having the two types of content treated differently
-            if 'text/plain' in (payload['content-type'] or payload._default_type):
-                parsed_payload += [email_cleaners.strip_html(payload._payload)]
+            """
+            Pull the payload text and send it to functions that strip out replies and html junk
+            Currently, we don't differentiate between how to process plaintext or html, because sometimes things labeled as plaintext still have html codes in them for some reason (email is frustrating and poorly standardized, you see)
+            There might be some optimization to be done in having the two types of content treated differently
+            """
+            if 'text/plain' in (payload['content-type'] or (not payload['content-type'] and 'text/plain' in payload._default_type)):
+                parsed_payload = email_cleaners.strip_html(payload._payload)
             elif 'text/html' in payload['content-type']:
-                parsed_payload += [email_cleaners.strip_html(payload._payload)]
+                parsed_payload = email_cleaners.strip_html(payload._payload)
         return parsed_payload
     except:
         return ''
 
-voice = raw_input('Target Email Address:')
-#config.txt should be a 2-line file containing the user login, followed by the user password
-#   note that gmail wants bots (like this one!) to use a special application-specific password that's generated for only this app
-login_details = open('config.txt','r')
-login = login_details.readline()
-password = login_details.readline()
 
-#IMAP Login Stuff. Change tihs stuff if using a different mail service
-M = imaplib.IMAP4_SSL('imap.gmail.com')
-M.login(login, password)
-#Select the mailbox. "[Gmail]/All Mail" gives all mail on the server, but there are other options for Gmail one can look up
-M.select('"[Gmail]/All Mail"', readonly = True)
-#Pull all emails from the target address from the user's All Mail mailbox
-typ, data = M.search(None, '(FROM "%s")' % (voice))
+def collect_imap_details():
+    """
+    Gets specifics for logging in and what emails we're interested in, then opens an IMAP connection and returns the
+    IMAP object.
+    config.txt should be a 2-line file containing the user login, followed by the user password. Note that gmail wants
+    bots (like this one!) to use a special application-specific password that's generated for only for this app.
+    :return M: IMAP object for interacting with IMAP server
+    :return voice: Defines which emails we'll pull. Emails we're pulling were sent from this address
+    """
+    voice = raw_input('Target Email Address:')
 
-#open textfile in the local folder
-textfile = open('All Emails From ' + voice +'.txt','w')
+    login_details = open('config.txt','r')
+    login = login_details.readline()
+    password = login_details.readline()
 
-for num in data[0].split():
-    #Grab the content from each email pulled - RFC822 is an email standard that includes content info, among other things
-    typ, data = M.fetch(num, '(RFC822)')
+    #IMAP Login Stuff. Change this stuff if using a different mail service
+    M = imaplib.IMAP4_SSL('imap.gmail.com')
+    M.login(login, password)
 
-    #Parser turns the nonsense we got from the above fetch command into an object with a .payload attribute, so we can just get the actual content
-    parsed_email = Parser().parsestr(data[0][1])
-    #We send the parsed email on to the payload parser, where we get the actual content and clean it up before sending it to be written to a textfile
-    parsed_payload = payload_parser(parsed_email)
+    return M, voice
 
-    try:
-        textfile.write(parsed_payload[0])
-    except:
-        pass
+
+def get_emails(M, voice):
+    """
+    Selects the mailbox, then pulls all emails from the target address from that mailbox.
+    The IMAP object and the pulled data are both returned.
+    The "[Gmail]/All Mail" mailbox gives all mail on the server, but there are other options for Gmail one can look up.
+    :param M: IMAP object for interacting with IMAP server
+    :param voice: Emails we're pulling were sent from this address
+    :return M: IMAP object for interacting with IMAP server, now in a 'selected' state
+    :return data: Data from IMAP server describing which emails were sent from voice
+    """
+    M.select('"[Gmail]/All Mail"', readonly = True)
+    typ, data = M.search(None, '(FROM "%s")' % (voice))
+    return M, data
+
+
+def parse_and_write_emails(M, data, voice):
+    """
+    Opens a textfile, parses the content from each email in 'data', writes that content to textfile, then closes it.
+    :param M: IMAP object for interacting with IMAP server
+    :param data: Data from IMAP server describing which emails were sent from voice
+    :param voice: Emails we're pulling were sent from this address
+    :return: None
+    """
+
+    textfile = open('All Emails From ' + voice +'.txt','w')
+
+    for num in data[0].split():
+        #RFC822 is an email standard that includes content info, among other things
+        typ, data = M.fetch(num, '(RFC822)')
+        #Parser turns the nonsense we got from the above fetch command into an object with a .payload attribute,
+        #  so we can just get the actual content
+        parsed_email = Parser().parsestr(data[0][1])
+        #We send the parsed email on to the payload parser,
+        # where we get the actual content and clean it up before sending it to be written to a textfile
+        parsed_payload = payload_parser(parsed_email)
+
+        try:
+            textfile.write(parsed_payload)
+        except:
+            pass
+
+    textfile.close()
+    return
+
+M, voice = collect_imap_details()
+M, data = get_emails(M, voice)
+parse_and_write_emails(M, data, voice)
 
 M.close()
 M.logout()
-textfile.close()
